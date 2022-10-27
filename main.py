0e3962d80a0e7c833261b4d168964c42de4a12fa
Braulio Hernandez <113930423+BraulioH@users.noreply.github.com>
2022-10-27 17:58:25 +0000

    working on returnStuff

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # from methods_help import method_help
-from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers, greet, welcome
-#from returnStuff import returnS
+# from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers, greet, welcome
+from returnStuff import power
 
 # method_help()
-function()
-#yogurt = welcome("Yogurt")
-added = add_numbers(4, 5)
-print(added)
-final = num_dividedFrom_add_numbers (45,added)
-print(final)
-braulio = welcome("Braulio")
-# returnS()
+# function()
+# #yogurt = welcome("Yogurt")
+# added = add_numbers(4, 5)
+# print(added)
+# final = num_dividedFrom_add_numbers (45,added)
+# print(final)
+# braulio = welcome("Braulio")
+power = power(4, 5)
