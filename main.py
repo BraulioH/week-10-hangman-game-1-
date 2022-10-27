a04b029db53bf3e3ffc38e1e1bda9b55e3883cd0
Braulio Hernandez <113930423+BraulioH@users.noreply.github.com>
2022-10-27 17:43:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# from methods_help import method_help
+from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers, greet, welcome
+#from returnStuff import returnS
+
+# method_help()
+function()
+#yogurt = welcome("Yogurt")
+added = add_numbers(4, 5)
+print(added)
+final = num_dividedFrom_add_numbers (45,added)
+print(final)
+braulio = welcome("Braulio")
+# returnS()
